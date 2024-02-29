a73d82673781b3989a92b55c7faf4e76e5821d20
Ilhom Jabborov <127211749+JabborovRoboCoder@users.noreply.github.com>
2024-02-29 18:26:20 +0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from fastai.vision.all import *
 import plotly.express as px
 
-st.title('Salom')
+st.title("Ko‘krak qafasi rentgenogrammasi yordamida COVID-19,Pneumonia va Sog'lom insonni aniqlash")
 
 file=st.file_uploader("Rasm yuklash",type=['png','jpeg','svg','jpg'])
 
